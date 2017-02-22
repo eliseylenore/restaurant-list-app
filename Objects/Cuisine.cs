1e740bfe56d07d5f8d57e16c50542d25b2527766
Elise St Hilaire and sandro mijatovic <elise+sandro@>
2017-02-22 14:06:09 -0800

    Add update() method

--- Objects/Cuisine.cs
+++ Objects/Cuisine.cs
@@ -175,5 +175,40 @@ namespace RestaurantListApp
       }
       return restaurants;
     }
+
+    public void Update(string newName)
+{
+  SqlConnection conn = DB.Connection();
+  conn.Open();
+
+  SqlCommand cmd = new SqlCommand("UPDATE cuisines SET name = @NewName OUTPUT INSERTED.name WHERE id = @CuisineId;", conn);
+
+  SqlParameter newNameParameter = new SqlParameter();
+  newNameParameter.ParameterName = "@NewName";
+  newNameParameter.Value = newName;
+  cmd.Parameters.Add(newNameParameter);
+
+
+  SqlParameter cuisineIdParameter = new SqlParameter();
+  cuisineIdParameter.ParameterName = "@CuisineId";
+  cuisineIdParameter.Value = this.GetId();
+  cmd.Parameters.Add(cuisineIdParameter);
+  SqlDataReader rdr = cmd.ExecuteReader();
+
+  while(rdr.Read())
+  {
+    this._name = rdr.GetString(0);
+  }
+
+  if (rdr != null)
+  {
+    rdr.Close();
+  }
+
+  if (conn != null)
+  {
+    conn.Close();
+  }
+}
   }
 }
--- Tests/CuisineTest.cs
+++ Tests/CuisineTest.cs
@@ -6,102 +6,119 @@ using System.Data.SqlClient;
 
 namespace RestaurantListApp
 {
-  public class CuisineTest : IDisposable
-  {
-    public CuisineTest()
+    public class CuisineTest : IDisposable
     {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=food_test;Integrated Security=SSPI;";
+        public CuisineTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=food_test;Integrated Security=SSPI;";
+        }
+
+        [Fact]
+        public void Test_CuisineEmptyAtFirst()
+        {
+            //Arrange, Act
+            int result = Cuisine.GetAll().Count;
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test_Equal_ReturnsTrueForSameName()
+        {
+            //Arrange, Act
+            Cuisine firstCuisine = new Cuisine("mexican");
+            Cuisine secondCuisine = new Cuisine("mexican");
+
+            //Assert
+            Assert.Equal(firstCuisine, secondCuisine);
+        }
+
+        [Fact]
+        public void Test_Save_SavesCuisineToDatabase()
+        {
+            //Arrange
+            Cuisine testCuisine = new Cuisine("mexican");
+            testCuisine.Save();
+
+            //Act
+            List<Cuisine> result = Cuisine.GetAll();
+            List<Cuisine> testList = new List<Cuisine>{testCuisine};
+
+            //Assert
+            Assert.Equal(testList, result);
+        }
+
+        [Fact]
+        public void Test_Save_AssignsIdToCuisineObject()
+        {
+            //Arrange
+            Cuisine testCuisine = new Cuisine("mexican");
+            testCuisine.Save();
+
+            //Act
+            Cuisine savedCuisine = Cuisine.GetAll()[0];
+
+            int result = savedCuisine.GetId();
+            int testId = testCuisine.GetId();
+
+            //Assert
+            Assert.Equal(testId, result);
+        }
+
+        [Fact]
+        public void Test_Find_FindsCuisineInDatabase()
+        {
+            //Arrange
+            Cuisine testCuisine = new Cuisine("mexican");
+            testCuisine.Save();
+
+            //Act
+            Cuisine foundCuisine = Cuisine.Find(testCuisine.GetId());
+
+            //Assert
+            Assert.Equal(testCuisine, foundCuisine);
+        }
+
+        [Fact]
+        public void Test_GetRestaurant_RetrievesAllRestaurantsWithCuisine()
+        {
+            Cuisine testCuisine = new Cuisine("mexican");
+            testCuisine.Save();
+
+            Restaurant firstRestaurant = new Restaurant("Larry's Tacos", 3, "Bend", testCuisine.GetId());
+            firstRestaurant.Save();
+            Restaurant secondRestaurant = new Restaurant("Brandon's Burritos", 2, "Mill Creek", testCuisine.GetId());
+            secondRestaurant.Save();
+
+
+            List<Restaurant> testRestaurantList = new List<Restaurant> {firstRestaurant, secondRestaurant};
+            List<Restaurant> resultRestaurantList = testCuisine.GetRestaurants();
+
+            Assert.Equal(testRestaurantList, resultRestaurantList);
+        }
+
+        [Fact]
+        public void Test_Update_UpdatesCuisineInDatabase()
+        {
+            //Arrange
+            string name = "Taco";
+            Cuisine testCuisine = new Cuisine(name);
+            testCuisine.Save();
+            string newName = "mexican";
+
+            //Act
+            testCuisine.Update(newName);
+
+            string result = testCuisine.GetName();
+
+            //Assert
+            Assert.Equal(newName, result);
+        }
+        public void Dispose()
+        {
+            Restaurant.DeleteAll();
+            Cuisine.DeleteAll();
+        }
     }
-
-    [Fact]
-    public void Test_CuisineEmptyAtFirst()
-    {
-      //Arrange, Act
-      int result = Cuisine.GetAll().Count;
-
-      //Assert
-      Assert.Equal(0, result);
-    }
-
-    [Fact]
-    public void Test_Equal_ReturnsTrueForSameName()
-    {
-      //Arrange, Act
-      Cuisine firstCuisine = new Cuisine("mexican");
-      Cuisine secondCuisine = new Cuisine("mexican");
-
-      //Assert
-      Assert.Equal(firstCuisine, secondCuisine);
-    }
-
-    [Fact]
-    public void Test_Save_SavesCuisineToDatabase()
-    {
-      //Arrange
-      Cuisine testCuisine = new Cuisine("mexican");
-      testCuisine.Save();
-
-      //Act
-      List<Cuisine> result = Cuisine.GetAll();
-      List<Cuisine> testList = new List<Cuisine>{testCuisine};
-
-      //Assert
-      Assert.Equal(testList, result);
-    }
-
-    [Fact]
-    public void Test_Save_AssignsIdToCuisineObject()
-    {
-      //Arrange
-      Cuisine testCuisine = new Cuisine("mexican");
-      testCuisine.Save();
-
-      //Act
-      Cuisine savedCuisine = Cuisine.GetAll()[0];
-
-      int result = savedCuisine.GetId();
-      int testId = testCuisine.GetId();
-
-      //Assert
-      Assert.Equal(testId, result);
-    }
-
-    [Fact]
-    public void Test_Find_FindsCuisineInDatabase()
-    {
-      //Arrange
-      Cuisine testCuisine = new Cuisine("mexican");
-      testCuisine.Save();
-
-      //Act
-      Cuisine foundCuisine = Cuisine.Find(testCuisine.GetId());
-
-      //Assert
-      Assert.Equal(testCuisine, foundCuisine);
-    }
-
-    [Fact]
-   public void Test_GetRestaurant_RetrievesAllRestaurantsWithCuisine()
-   {
-     Cuisine testCuisine = new Cuisine("mexican");
-     testCuisine.Save();
-
-     Restaurant firstRestaurant = new Restaurant("Larry's Tacos", 3, "Bend", testCuisine.GetId());
-     firstRestaurant.Save();
-     Restaurant secondRestaurant = new Restaurant("Brandon's Burritos", 2, "Mill Creek", testCuisine.GetId());
-     secondRestaurant.Save();
-
-
-     List<Restaurant> testRestaurantList = new List<Restaurant> {firstRestaurant, secondRestaurant};
-     List<Restaurant> resultRestaurantList = testCuisine.GetRestaurants();
-
-     Assert.Equal(testRestaurantList, resultRestaurantList);
-   }
-
-    public void Dispose()
-    {
-      Restaurant.DeleteAll();
-      Cuisine.DeleteAll();
-    }
-  }
 }
